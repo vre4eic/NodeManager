f2125fa049366d20a561e40111bef6137c7ecbe9
Cesare <cesare.concordia@isti.cnr.it>
2017-04-04 11:30:04 +0200

    minor changes

--- src/main/java/eu/vre4eic/evre/nodeservice/usermanager/impl/TgAuthenticator.java
+++ src/main/java/eu/vre4eic/evre/nodeservice/usermanager/impl/TgAuthenticator.java
@@ -1,29 +1,36 @@
 package eu.vre4eic.evre.nodeservice.usermanager.impl;
 
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 public class TgAuthenticator extends TelegramLongPollingBot {
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+	Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+	
+	//private Logger log = LoggerFactory.getLogger(this.getClass());
+
 	
     @Override
     public void onUpdateReceived(Update update) {
+    	root.setLevel(Level.INFO);
     	 if (update.hasMessage() && update.getMessage().hasText()) {
     	        SendMessage message = new SendMessage() 
     	                .setChatId(update.getMessage().getChatId())
     	                .setText(update.getMessage().getText());
     	        try {
-    	        	log.info("************************** ");
-    	        	log.info(" "+message.getChatId());
-    	        	log.info("************************** ");
+    	        	
+    	        	root.info("************************** ");
+    	        	root.info(" "+message.getChatId());
+    	        	root.info("************************** ");
     	            sendMessage(message); // Call method to send the message
     	        } catch (TelegramApiException e) {
     	            e.printStackTrace();
-    	            log.error(e.toString());
+    	            root.error(e.toString());
     	        }
     	    }
     }
